453045c5d5b5c001cbfd595dce0d9d77d579ef0c
Eric Olander <eolander@the41st.com>
2015-03-08 19:47:13 -0700

    update to handle suspensions

--- src/main/scala/chord/ShellChord.scala
+++ src/main/scala/chord/ShellChord.scala
@@ -4,7 +4,8 @@ package chord
  * Created by eolander on 12/28/14.
  */
 class ShellChord(chord: Chord)
-extends Chord(chord.name, chord.root, chord.triad, chord.quality, chord.extension, chord.alteration, chord.added, chord.altRoot) {
+extends Chord(chord.name, chord.root, chord.triad, chord.quality, chord.extension, chord.alteration, chord.added,
+              chord.suspension, chord.altRoot) {
 
   override def intervals(extensions: => List[String] = List(extension.toString)): List[String] = {
     chord.intervals(extensions).filterNot(_.contains("5"))
