f4eb6d438aa4b5d6ea5c066579ed9d86d0950f73
Eric Olander <olandere@gmail.com>
2017-07-11 21:42:49 -0600

    Better use of scalatest dsl

--- src/test/scala/chord/TuningSpec.scala
+++ src/test/scala/chord/TuningSpec.scala
@@ -5,31 +5,31 @@ import org.scalatest._
 /**
  * Created by eolander on 2/25/15.
  */
-class TuningSpec extends FlatSpec {
+class TuningSpec extends FlatSpec with Matchers {
 
   "A tuning" should "not case about case" in {
     val t = Tuning("a b c d e")
-    assert(t.root == "A")
-    assert(t.numStrings == 5)
-    assert(t.semitones == List(0,2,3,5,7))
+    t.root shouldBe "A"
+    t.numStrings shouldBe 5
+    t.semitones shouldBe List(0,2,3,5,7)
   }
 
   it should "handle sharps and flats" in {
     val t = Tuning("a b c# d e♭")
-    assert(t.root == "A")
-    assert(t.numStrings == 5)
-    assert(t.semitones == List(0,2,4,5,6))
+    t.root shouldBe "A"
+    t.numStrings shouldBe 5
+    t.semitones shouldBe List(0,2,4,5,6)
   }
 
   it should "not case about whitespace" in {
     val t = Tuning(" a   b c   d eb  ")
-    assert(t.root == "A")
-    assert(t.numStrings == 5)
-    assert(t.semitones == List(0,2,3,5,6))
+    t.root shouldBe "A"
+    t.numStrings shouldBe 5
+    t.semitones shouldBe List(0,2,3,5,6)
   }
 
   it should "print the tuning" in {
     val t = Tuning("C# G# C# G# C# E")
-    assert(t.toString == "C# G# C# G# C# E")
+    t.toString shouldBe "C# G# C# G# C# E"
   }
 }
