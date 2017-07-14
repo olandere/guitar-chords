6d04b3f7797850efb3ac813b21d93d5ba713b732
Eric Olander <olandere@gmail.com>
2017-07-14 13:56:01 -0600

    updates for scale generation

--- src/main/scala/chord/Scale.scala
+++ src/main/scala/chord/Scale.scala
@@ -15,26 +15,33 @@ sealed trait Scale {
   }
 
   //val preferSharps = Set("G", "D", "A", "E", "B").contains(root) || root.contains("♯")
-  val notes: Map[Int, Note] = {
-    val mapping = reverseNoteMap(root)
-    if (root.accidental != Sharp() && intervals.map(mapping).toSet.size != 7) {
+  val noteMap: Map[Int, Note] = {
+    val mapping = reverseNoteMap(root, !CircleOfFifths.flatKeys.contains(root))
+    if (intervals.map(mapping).toSet.size != 7) {
       reverseNoteMap(root, preferSharps = false)
     } else {
       mapping
     }
   }
 
+  def notes: Seq[Note] = for {i <- intervals} yield noteMap(i)
 
   def relatedScale: Scale
+
+  def rotateLeft[A](l: List[A], n: Int = 1):List[A] = l.drop(n) ++ l.take(n)
+
+  def major = List(2,2,1,2,2,2,1)
+  
+  def genIntervals(n: Int): List[Int] = rotateLeft(major, n).scanLeft(0)(_+_).take(7)
 }
 
 case class MajorScale(root: Note) extends Scale {
 
-  def intervals: List[Int] = List(0, 2, 4, 5, 7, 9, 11)//List(2,2,1,2,2,2,1)
+  def intervals: List[Int] = genIntervals(0)
 
   def relatedScale: Scale = this
 
-  def relativeMinor: Scale = Aeolian(notes(9))
+  def relativeMinor: Scale = Aeolian(noteMap(9))
 }
 
 case class HarmonicMinor(root: Note) extends Scale {
@@ -46,23 +53,23 @@ case class HarmonicMinor(root: Note) extends Scale {
 
 case class Dorian(root: Note) extends Scale {
 
-  def intervals: List[Int] = List(0, 2, 3, 5, 7, 9, 10)//List(2,1,2,2,2,1,2)
+  def intervals: List[Int] = genIntervals(1)//List(2,1,2,2,2,1,2)
 
-  def relatedScale: Scale = MajorScale(notes(10))
+  def relatedScale: Scale = MajorScale(noteMap(10))
 }
 
 case class Phrygian(root: Note) extends Scale {
 
-  def intervals: List[Int] = List(0, 1, 3, 5, 7, 8, 10)
+  def intervals: List[Int] = genIntervals(2)
 
-  def relatedScale: Scale = MajorScale(notes(8))
+  def relatedScale: Scale = MajorScale(noteMap(8))
 }
 
 case class PhrygianDominant(root: Note) extends Scale {
 
   def intervals: List[Int] = List(0, 1, 4, 5, 7, 8, 10)
 
-  def relatedScale: Scale = HarmonicMinor(notes(5))
+  def relatedScale: Scale = HarmonicMinor(noteMap(5))
 }
 
 case class DoubleHarmonic(root: Note) extends Scale {
@@ -74,58 +81,58 @@ case class DoubleHarmonic(root: Note) extends Scale {
 
 case class Lydian(root: Note) extends Scale {
 
-  def intervals: List[Int] = List(0, 2, 4, 6, 7, 9, 11)
+  def intervals: List[Int] = genIntervals(3)
 
-  def relatedScale: Scale = MajorScale(notes(7))
+  def relatedScale: Scale = MajorScale(noteMap(7))
 }
 
 case class LydianDominant(root: Note) extends Scale {
 
   def intervals: List[Int] = List(0, 2, 4, 6, 7, 9, 10)
 
-  def relatedScale: Scale = MajorScale(notes(7))
+  def relatedScale: Scale = MajorScale(noteMap(7))
 }
 
 case class Mixolydian(root: Note) extends Scale {
 
-  def intervals: List[Int] = List(0, 2, 4, 5, 7, 9, 10)
+  def intervals: List[Int] = genIntervals(4)
 
-  def relatedScale: Scale = MajorScale(notes(5))
+  def relatedScale: Scale = MajorScale(noteMap(5))
 }
 
 case class Aeolian(root: Note) extends Scale {
 
-  def intervals: List[Int] = List(0, 2, 3, 5, 7, 8, 10)
+  def intervals: List[Int] = genIntervals(5)
 
-  def relatedScale: Scale = MajorScale(notes(3))
+  def relatedScale: Scale = MajorScale(noteMap(3))
 }
 
 case class Locrian(root: Note) extends Scale {
 
-  def intervals: List[Int] = List(0, 1, 3, 5, 6, 8, 10)
+  def intervals: List[Int] = genIntervals(6)
 
-  def relatedScale: Scale = MajorScale(notes(1))
+  def relatedScale: Scale = MajorScale(noteMap(1))
 }
 
 case class SuperLocrian(root: Note) extends Scale {
 
   def intervals: List[Int] = List(0, 1, 3, 4, 6, 8, 10)
 
-  def relatedScale: Scale = MajorScale(notes(1))
+  def relatedScale: Scale = MajorScale(noteMap(1))
 }
 
 case class MinorPent(root: Note) extends Scale {
 
   def intervals: List[Int] = List(0, 3, 5, 7, 10)
 
-  def relatedScale: Scale = MajorScale(notes(3))
+  def relatedScale: Scale = MajorScale(noteMap(3))
 }
 
 case class MajorPent(root: Note) extends Scale {
 
   def intervals: List[Int] = List(0, 2, 4, 7, 9)
 
-  def relatedScale: Scale = MajorScale(notes(0))
+  def relatedScale: Scale = MajorScale(noteMap(0))
 }
 
 object Scale {
